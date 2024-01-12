8d57a047bbe7a95caefb6b1f520cd6951456b40a
Stilian Stefanov <106154612+stilianstefanov@users.noreply.github.com>
2024-01-12 15:44:15 +0200

    Created WarehousesController

---
+++ src/WarehouseService/Controllers/WarehousesController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WarehouseService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WarehousesController : ControllerBase
+    {
+
+    }
+}
