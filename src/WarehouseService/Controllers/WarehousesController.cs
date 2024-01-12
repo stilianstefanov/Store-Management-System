d7b37a2a1d7961e9854cea7c370aee2c02d6f779
Stilian Stefanov <106154612+stilianstefanov@users.noreply.github.com>
2024-01-12 19:18:11 +0200

    Created GetAll and Create actions

--- src/WarehouseService/Controllers/WarehousesController.cs
+++ src/WarehouseService/Controllers/WarehousesController.cs
@@ -4,11 +4,32 @@ using Microsoft.AspNetCore.Mvc;
 namespace WarehouseService.Controllers
 {
     using Data.Contracts;
+    using Data.ViewModels;
+    using Services.Contracts;
 
     [Route("api/[controller]")]
     [ApiController]
     public class WarehousesController : ControllerBase
     {
-        
+        private readonly IWarehouseService _warehouseService;
+
+        public WarehousesController(IWarehouseService warehouseService)
+        {
+            _warehouseService = warehouseService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllAsync()
+        {
+            return Ok(await _warehouseService.GetAllAsync());
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateAsync(WarehouseCreateModel model)
+        {
+            await _warehouseService.CreateAsync(model);
+
+            return StatusCode(StatusCodes.Status201Created);
+        }
     }
 }
