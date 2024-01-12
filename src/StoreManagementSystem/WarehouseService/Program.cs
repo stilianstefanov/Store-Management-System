504c1f9d7da02e5af05339094ab82e66cca5c4a6
Stilian Stefanov <106154612+stilianstefanov@users.noreply.github.com>
2024-01-12 15:37:23 +0200

    Initial commit

--- src/StoreManagementSystem/WarehouseService/Program.cs
+++ src/StoreManagementSystem/WarehouseService/Program.cs
@@ -1,21 +1,19 @@
 
-namespace ProductService
+namespace WarehouseService
 {
     public class Program
     {
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
-            
-            
+
             builder.Services.AddControllers();
-            
+         
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
 
             var app = builder.Build();
 
-            
             if (app.Environment.IsDevelopment())
             {
                 app.UseSwagger();
