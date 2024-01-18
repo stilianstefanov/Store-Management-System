3d6f1bb7a0d90f39f544618463567a0863116199
Stilian Stefanov <106154612+stilianstefanov@users.noreply.github.com>
2024-01-18 17:05:22 +0200

    Registered DbContext with InMemory Db

--- src/ProductService/Program.cs
+++ src/ProductService/Program.cs
@@ -1,22 +1,23 @@
-
 namespace ProductService
 {
+    using Data;
+    using Microsoft.EntityFrameworkCore;
+
     public class Program
     {
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
 
-            // Add services to the container.
+            builder.Services.AddDbContext<ProductDbContext>(opt =>
+                opt.UseInMemoryDatabase("TestDb"));
 
             builder.Services.AddControllers();
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
 
             var app = builder.Build();
 
-            // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
             {
                 app.UseSwagger();
