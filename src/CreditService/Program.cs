64a747a27fc117368683b68b564befa0cc29f260
Stilian Stefanov <106154612+stilianstefanov@users.noreply.github.com>
2024-02-05 12:16:58 +0200

    Registered CreditDbContext with InMem db for testing

--- src/CreditService/Program.cs
+++ src/CreditService/Program.cs
@@ -1,22 +1,26 @@
 
 namespace CreditService
 {
+    using Data;
+    using Microsoft.EntityFrameworkCore;
+
     public class Program
     {
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
 
-            // Add services to the container.
+            builder.Services.AddDbContext<CreditDbContext>(options =>
+                options.UseInMemoryDatabase("TestDb"));
 
             builder.Services.AddControllers();
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+  
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
 
             var app = builder.Build();
 
-            // Configure the HTTP request pipeline.
+
             if (app.Environment.IsDevelopment())
             {
                 app.UseSwagger();
