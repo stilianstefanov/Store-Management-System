1d3b4629766d44ff1dbe9621c6b43311c0c954ec
Stilian Stefanov <106154612+stilianstefanov@users.noreply.github.com>
2024-02-05 12:06:09 +0200

    Created model Borrower

---
+++ src/CreditService/Data/Models/Borrower.cs
@@ -0,0 +1,23 @@
+﻿namespace CreditService.Data.Models
+{
+    using System.ComponentModel.DataAnnotations;
+
+    public class Borrower
+    {
+        public Borrower()
+        {
+            Id = Guid.NewGuid();
+        }
+
+        [Key]
+        public Guid Id { get; set; }
+
+        public string Name { get; set; } = null!;
+
+        public string? Surname { get; set; }
+
+        public string LastName { get; set; } = null!;
+
+        public decimal CreditLimit { get; set; }
+    }
+}
