f4ee75c448178afec55259a6d92c5322a03d978c
Stilian Stefanov <106154612+stilianstefanov@users.noreply.github.com>
2024-02-10 17:33:57 +0200

    Created PurchaseProductsController

---
+++ src/CreditService/Controllers/PurchaseProductsController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CreditService.Controllers
+{
+    [Route("api/borrowers/{borrowerId}/purchases/{purchaseId}/[controller]")]
+    [ApiController]
+    public class PurchaseProductsController : ControllerBase
+    {
+    }
+}
