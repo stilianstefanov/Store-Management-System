d4e23652572413459f22dc948d5d2a890c2e0dd4
Stilian Stefanov <106154612+stilianstefanov@users.noreply.github.com>
2024-02-17 15:24:05 +0200

    Created OperationResult and ControllerExtensions

---
+++ src/ProductService/Utilities/Enums/ControllerExtensions.cs
@@ -0,0 +1,13 @@
+﻿namespace ProductService.Utilities.Enums
+{
+    using Microsoft.AspNetCore.Mvc;
+    using static Common.ExceptionMessages;
+
+    public static class ControllerExtensions
+    {
+        public static IActionResult GeneralError(this ControllerBase controller)
+        {
+            return controller.StatusCode(StatusCodes.Status500InternalServerError, new { Error = GeneralErrorMessage });
+        }
+    }
+}
