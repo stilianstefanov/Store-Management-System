807bcf6f498da2e7fc211e88a253f68756e9ca69
Stilian Stefanov <106154612+stilianstefanov@users.noreply.github.com>
2024-02-21 18:53:28 +0200

    Implemented AuthController (Login, Register)

---
+++ src/IdentityService/Controllers/AuthController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IdentityService.Controllers
+{
+    using Data.ViewModels;
+    using Services.Contracts;
+    using Utilities;
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly IAuthService _authService;
+
+        public AuthController(IAuthService authService)
+        {
+            _authService = authService;
+        }
+
+        [HttpPost]
+        [Route(nameof(Register))]
+        public async Task<IActionResult> Register([FromBody] RegisterModel registerModel)
+        {
+            var result = await _authService.RegisterAsync(registerModel);
+
+            if (!result.IsSuccess) return this.Error(result.ErrorType, result.ErrorMessage!);
+
+            return this.Ok(result.Data);
+        }
+
+        [HttpPost]
+        [Route(nameof(Login))]
+        public async Task<IActionResult> Login([FromBody] LoginModel loginModel)
+        {
+            var result = await _authService.LoginAsync(loginModel);
+
+            if (!result.IsSuccess) return this.Error(result.ErrorType, result.ErrorMessage!);
+
+            return this.Ok(result.Data);
+        }
+    }
+}
