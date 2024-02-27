4d62042498e4505af566c80681709d4f6c3c0cd2
Stilian Stefanov <106154612+stilianstefanov@users.noreply.github.com>
2024-02-27 13:48:09 +0200

    Added UserId property to borrower

--- src/CreditService/Data/Migrations/20240227114517_AddedUserIdPropertyToBorrower.Designer.cs
+++ src/CreditService/Data/Migrations/20240227114517_AddedUserIdPropertyToBorrower.Designer.cs
@@ -4,6 +4,7 @@ using CreditService.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace CreditService.Migrations
 {
     [DbContext(typeof(CreditDbContext))]
-    partial class CreditDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240227114517_AddedUserIdPropertyToBorrower")]
+    partial class AddedUserIdPropertyToBorrower
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
