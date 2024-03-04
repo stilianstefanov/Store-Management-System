72b638d504857b97f3b80e358f60fd6f03142a28
Stilian Stefanov <106154612+stilianstefanov@users.noreply.github.com>
2024-03-04 15:55:03 +0200

    Added home image, updated styles

--- client/src/pages/home/Home.js
+++ client/src/pages/home/Home.js
@@ -1,5 +1,13 @@
+import image from '../../images/home.jpg'
+import styles from './Home.module.css'
+
 function HomePage() {
-    return <div>Test Home Page</div>
+    return (
+    <div>
+        <img className={styles.image} src={image} alt='homeimage'/>
+        <h1 className={styles.inscription}>Manage your store easy and secure!</h1>
+    </div>
+    );
 }
 
 export default HomePage;
