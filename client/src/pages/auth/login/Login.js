e44e5ebff27be0acfc0a1e86dcce4b5f98b01c09
Stilian Stefanov <106154612+stilianstefanov@users.noreply.github.com>
2024-03-06 12:51:17 +0200

    Created login form added styles

--- client/src/pages/auth/login/Login.js
+++ client/src/pages/auth/login/Login.js
@@ -1,5 +1,42 @@
-function LoginPage() {
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import styles from "./Login.module.css"
 
+function LoginPage() {
+    const navigate = useNavigate();
+    const [emailOrUserName, setEmailOrUserName] = useState("");
+    const [password, setPassword] = useState("");
+
+    function handleLogin() {
+
+    }
+
+
+    return (
+        <div className={styles["Auth-container"]}>
+            <div className={styles["Auth-card"]}>
+                <h1 className={styles["Auth-header"]}>Login</h1>
+                <form>
+                    <input
+                        placeholder="Email or Username"
+                        className={styles["Auth-input"]}
+                        value={emailOrUserName}
+                        onChange={(e) => setEmailOrUserName(e.target.value)}
+                    />
+                    <input
+                        type="password"
+                        placeholder="Password"
+                        className={styles["Auth-input"]}
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                    />
+                    <button type="button" className={styles["Auth-button"]} onClick={handleLogin}>
+                        Login
+                    </button>
+                </form>
+            </div>
+        </div>
+    );
 }
 
 export default LoginPage;
--- client/src/pages/auth/register/Register.js
+++ client/src/pages/auth/register/Register.js
@@ -1,5 +1,5 @@
 function RegisterPage() {
-
+    
 }
 
 export default RegisterPage;
