79821174d00c9284b38b82f33dcf6000caaa0767
Stilian Stefanov <106154612+stilianstefanov@users.noreply.github.com>
2024-03-10 13:42:49 +0200

    Added register function

--- client/src/services/UserService.js
+++ client/src/services/UserService.js
@@ -5,4 +5,9 @@ const baseUrl = 'http://acme.com/api/Auth';
 export const Login = async (loginRequest) => {
     const response = await axios.post(`${baseUrl}/login`, loginRequest);
     return response.data;
- }
+ };
+
+export const Register = async (registerRequest) => {
+    const response = await axios.post(`${baseUrl}/register`, registerRequest);
+    return response.data;
+};
