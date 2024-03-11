b842b768a346abb6ae8c9ae1a0986e523b532964
Stilian Stefanov <106154612+stilianstefanov@users.noreply.github.com>
2024-03-11 15:02:47 +0200

    Added Dashboard to homepage (when the user is logged)

--- client/src/pages/home/Home.js
+++ client/src/pages/home/Home.js
@@ -1,12 +1,23 @@
 import image from '../../images/home.jpg'
 import styles from './Home.module.css'
+import DashBoard from '../../components/dashboard/Dashboard';
+import { useAuth } from '../../context/AuthContext';
 
 function HomePage() {
+    const { isLoggedIn } = useAuth();
+
     return (
-    <div>      
-        <img className={styles.image} src={image} alt='homeimage'/>
-        <h1 className={styles.inscription}>Manage your store easy and secure!</h1>
-    </div>
+        <div>
+            {isLoggedIn ? (
+                <DashBoard />
+            ) : (
+                <div>
+                    <img className={styles.image} src={image} alt='homeimage' />
+                    <h1 className={styles.inscription}>Manage your store easy and secure!</h1>
+                </div>
+            )
+            }
+        </div>
     );
 }
 
