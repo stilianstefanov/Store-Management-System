701aac57c2201d1fc7c35b12472b8a962639e9d3
Stilian Stefanov <106154612+stilianstefanov@users.noreply.github.com>
2024-03-11 16:58:54 +0200

    Updated styles

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -4,12 +4,13 @@ import DashboardProduct from '../product/DashboardProduct';
 function DashBoard() {
     return (
         <div className={styles['Dash-container']}>
+            <h1>Dashboard</h1>
             <input 
                 placeholder='Barcode'
                 className={styles['Barcode-input']}
                 //ToDO Implement onChange
             />
-            <div>
+            <div className={styles['Products-header']}>
                 <h3>Product</h3>
                 <h3>Price</h3>
                 <h3>Quantity</h3>
