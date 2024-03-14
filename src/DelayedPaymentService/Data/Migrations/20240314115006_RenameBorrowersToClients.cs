e29fcc1cc535ae065ac48932be97e6709bd23b68
Stilian Stefanov <106154612+stilianstefanov@users.noreply.github.com>
2024-03-14 13:56:29 +0200

    Updated appsettings, applied migrations

---
+++ src/DelayedPaymentService/Data/Migrations/20240314115006_RenameBorrowersToClients.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CreditService.Migrations
+{
+    /// <inheritdoc />
+    public partial class RenameBorrowersToClients : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Purchases_Borrowers_BorrowerId",
+                table: "Purchases");
+
+            migrationBuilder.DropTable(
+                name: "Borrowers");
+
+            migrationBuilder.RenameColumn(
+                name: "BorrowerId",
+                table: "Purchases",
+                newName: "ClientId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Purchases_BorrowerId",
+                table: "Purchases",
+                newName: "IX_Purchases_ClientId");
+
+            migrationBuilder.CreateTable(
+                name: "Clients",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Surname = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: true),
+                    LastName = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    CurrentCredit = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    CreditLimit = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    IsDeleted = table.Column<bool>(type: "bit", nullable: false),
+                    UserId = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Clients", x => x.Id);
+                });
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Purchases_Clients_ClientId",
+                table: "Purchases",
+                column: "ClientId",
+                principalTable: "Clients",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Purchases_Clients_ClientId",
+                table: "Purchases");
+
+            migrationBuilder.DropTable(
+                name: "Clients");
+
+            migrationBuilder.RenameColumn(
+                name: "ClientId",
+                table: "Purchases",
+                newName: "BorrowerId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Purchases_ClientId",
+                table: "Purchases",
+                newName: "IX_Purchases_BorrowerId");
+
+            migrationBuilder.CreateTable(
+                name: "Borrowers",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    CreditLimit = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    CurrentCredit = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    IsDeleted = table.Column<bool>(type: "bit", nullable: false),
+                    LastName = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Surname = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: true),
+                    UserId = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Borrowers", x => x.Id);
+                });
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Purchases_Borrowers_BorrowerId",
+                table: "Purchases",
+                column: "BorrowerId",
+                principalTable: "Borrowers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
