0aff183bea27a0e8301a668c0a7b0d09d73cac71
Stilian Stefanov <106154612+stilianstefanov@users.noreply.github.com>
2024-03-25 13:42:33 +0200

    Created ClientPartialUpdateModel.cs

---
+++ src/DelayedPaymentService/Data/ViewModels/Client/ClientPartialUpdateModel.cs
@@ -0,0 +1,14 @@
+﻿namespace DelayedPaymentService.Data.ViewModels.Client
+{
+    using System.ComponentModel.DataAnnotations;
+    using static Common.EntityValidationConstants.Client;
+
+    public class ClientPartialUpdateModel
+    {
+        [Range(typeof(decimal), CurrentCreditMinValue, CurrentCreditMaxValue)]
+        public decimal? CurrentCredit { get; set; }
+
+        [Range(typeof(decimal), CreditLimitMinValue, CreditLimitMaxValue)]
+        public decimal? CreditLimit { get; set; }
+    }
+}
