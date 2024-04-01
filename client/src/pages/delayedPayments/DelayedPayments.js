b8908090adaf0fdddb8a9adc8f2d64466e7a5ff7
Stilian Stefanov <106154612+stilianstefanov@users.noreply.github.com>
2024-04-01 13:40:20 +0300

    Added table

--- client/src/pages/delayedPayments/DelayedPayments.js
+++ client/src/pages/delayedPayments/DelayedPayments.js
@@ -3,38 +3,52 @@ import styles from './DelayedPayments.module.css'
 function DelayedPaymentsPage() {
     return (
         <div className={`container ${styles['table-container']}`}>
-        <h1 className={`text-center ${styles['title']}`}>Clients</h1>
-        <div className="d-flex justify-content-center flex-wrap">
-            <div className={styles['input-group']}>
-                <label htmlFor="search-input">Search:</label>
-                <input
-                    id="search-input"
-                    type="text"
-                    placeholder="Search client"
-                    className={`form-control ${styles['input-field']}`}
-                />
+            <h1 className={`text-center ${styles['title']}`}>Clients</h1>
+            <div className="d-flex justify-content-center flex-wrap">
+                <div className={styles['input-group']}>
+                    <label htmlFor="search-input">Search:</label>
+                    <input
+                        id="search-input"
+                        type="text"
+                        placeholder="Search client"
+                        className={`form-control ${styles['input-field']}`}
+                    />
+                </div>
+                <div className={styles['input-group']}>
+                    <label htmlFor="order-select">Sort by:</label>
+                    <select id="order-select" className={`form-control ${styles['input-field']}`}>
+                        <option value="0">Name (Ascending)</option>
+                        <option value="1">Name (Descending)</option>
+                        <option value="2">Current credit (Descending)</option>
+                        <option value="3">Current credit (Ascending)</option>
+                        <option value="4">Credit limit (Descending)</option>
+                        <option value="4">Credit limit (Ascending)</option>
+                    </select>
+                </div>
+                <div className={styles['input-group']}>
+                    <label htmlFor="order-select">Clients per Page :</label>
+                    <select id="order-select" className={`form-control ${styles['input-field']}`}>
+                        <option value="0">10</option>
+                        <option value="1">15</option>
+                        <option value="2">20</option>
+                    </select>
+                </div>
             </div>
-            <div className={styles['input-group']}>
-                <label htmlFor="order-select">Sort by:</label>
-                <select id="order-select" className={`form-control ${styles['input-field']}`}>
-                    <option value="0">Name (Ascending)</option>
-                    <option value="1">Name (Descending)</option>
-                    <option value="2">Current credit (Descending)</option>
-                    <option value="3">Current credit (Ascending)</option>
-                    <option value="4">Credit limit (Descending)</option>
-                    <option value="4">Credit limit (Ascending)</option>
-                </select>
-            </div>
-            <div className={styles['input-group']}>
-                <label htmlFor="order-select">Clients per Page :</label>
-                <select id="order-select" className={`form-control ${styles['input-field']}`}>
-                    <option value="0">10</option>
-                    <option value="1">15</option>
-                    <option value="2">20</option>
-                </select>
-            </div>
+            <table className={styles['table-fill']}>
+                <thead>
+                    <tr>
+                        <th className={styles['text-left']}>Name</th>
+                        <th className={styles['text-left']}>Surname</th>
+                        <th className={styles['text-left']}>Lastname</th>
+                        <th className={styles['text-left']}>Current credit</th>
+                        <th className={styles['text-left']}>Credit limit</th>
+                    </tr>
+                </thead>
+                <tbody className={styles['table-hover']}>
+                    
+                </tbody>
+            </table>
         </div>
-    </div>
     );
 }
 
