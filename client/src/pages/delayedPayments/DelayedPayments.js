9844ee0a3d37732a09adbd9f564fd932a3a0b931
Stilian Stefanov <106154612+stilianstefanov@users.noreply.github.com>
2024-04-01 13:08:02 +0300

    Added "Order by" select item

--- client/src/pages/delayedPayments/DelayedPayments.js
+++ client/src/pages/delayedPayments/DelayedPayments.js
@@ -3,8 +3,30 @@ import styles from './DelayedPayments.module.css'
 function DelayedPaymentsPage() {
     return (
         <div className={`container ${styles['table-container']}`}>
-            <h1 className={`text-center ${styles['title']}`}>Clients</h1>
+        <h1 className={`text-center ${styles['title']}`}>Clients</h1>
+        <div className="d-flex justify-content-center flex-wrap">
+            <div className={styles['input-group']}>
+                <label htmlFor="search-input">Search:</label>
+                <input
+                    id="search-input"
+                    type="text"
+                    placeholder="Search client"
+                    className={`form-control ${styles['input-field']}`}
+                />
+            </div>
+            <div className={styles['input-group']}>
+                <label htmlFor="order-select">Order by:</label>
+                <select id="order-select" className={`form-control ${styles['input-field']}`}>
+                    <option value="0">Name (Ascending)</option>
+                    <option value="1">Name (Descending)</option>
+                    <option value="2">Current credit (Descending)</option>
+                    <option value="3">Current credit (Ascending)</option>
+                    <option value="4">Credit limit (Descending)</option>
+                    <option value="4">Credit limit (Ascending)</option>
+                </select>
+            </div>
         </div>
+    </div>
     );
 }
 
