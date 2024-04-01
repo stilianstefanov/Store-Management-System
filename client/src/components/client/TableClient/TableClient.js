e78d8efa75c4b0595cb1df351e411d061d3cc0f7
Stilian Stefanov <106154612+stilianstefanov@users.noreply.github.com>
2024-04-01 13:57:42 +0300

    Created TableClient.js component

---
+++ client/src/components/client/TableClient/TableClient.js
@@ -0,0 +1,15 @@
+import styles from './TableClient.module.css'
+
+function TableClient ({ client }) {
+    return (
+        <tr>
+            <td className={styles['text-left']}>{client.name}</td>
+            <td className={styles['text-left']}>{client.surname}</td>
+            <td className={styles['text-left']}>{client.lastname}</td>
+            <td className={styles['text-left']}>{client.currentCredit}</td>
+            <td className={styles['text-left']}>{client.creditLimit}</td>
+        </tr>
+    );
+};
+
+export default TableClient;
