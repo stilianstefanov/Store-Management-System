1a633f4b7f9b7f707a397e96d87ea6e554f62652
Stilian Stefanov <106154612+stilianstefanov@users.noreply.github.com>
2024-04-03 14:42:30 +0300

    Created enum for client sorting

---
+++ src/DelayedPaymentService/Data/ViewModels/Client/Enums/ClientSorting.cs
@@ -0,0 +1,12 @@
+﻿namespace DelayedPaymentService.Data.ViewModels.Client.Enums
+{
+    public enum ClientSorting
+    {
+        NameAscending = 0,
+        NameDescending = 1,
+        CurrentCreditDescending = 2,
+        CurrentCreditAscending = 3,
+        CreditLimitDescending = 4,
+        CreditLimitAscending = 5,
+    }
+}
