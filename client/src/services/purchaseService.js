5acd659641c9eb64b42b4c25b0e05f9ede7a9f7e
Stilian Stefanov <106154612+stilianstefanov@users.noreply.github.com>
2024-04-05 15:17:48 +0300

    Implemented GetAll request

--- client/src/services/purchaseService.js
+++ client/src/services/purchaseService.js
@@ -2,6 +2,18 @@ import axios from "axios";
 
 const baseUrl = 'http://acme.com/api/clients';
 
+export const GetAll = async (clientId, params) => {
+    const url = new URL(`http://acme.com/api/clients/${clientId}/Purchases`);
+    const config = { headers: { Authorization: `Bearer ${sessionStorage.getItem('token')}` } };
+
+    if (params) Object.entries(params).forEach(([key, value]) => {
+        if (value) url.searchParams.append(key, value);
+    });
+
+    const response = await axios.get(url, config);
+    return response.data;
+};
+
 export const CreatePurchase = async (products, clientId) => {
     const config = { headers: { Authorization: `Bearer ${sessionStorage.getItem('token')}` }};
 
