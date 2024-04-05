332298e02f7c88572a20fbea035edf8f3faaec84
Stilian Stefanov <106154612+stilianstefanov@users.noreply.github.com>
2024-04-05 13:59:19 +0300

    Added input fields for filtering

--- client/src/components/client/ClientDetails/ClientDetails.js
+++ client/src/components/client/ClientDetails/ClientDetails.js
@@ -1,6 +1,19 @@
 import styles from './ClientDetails.module.css'
 
 function ClientDetails({ client, closeClientDetails, refreshClients }) {
+    const testPurchases = [
+        { date: '19-12-2022', amount: 20.54 },
+        { date: '19-12-2022', amount: 20.54 },
+        { date: '19-12-2022', amount: 20.54 },
+        { date: '19-12-2022', amount: 20.54 },
+        { date: '19-12-2022', amount: 20.54 },
+        { date: '19-12-2022', amount: 20.54 },
+        { date: '19-12-2022', amount: 20.54 },
+        { date: '19-12-2022', amount: 20.54 },
+        { date: '19-12-2022', amount: 20.54 },
+        { date: '19-12-2022', amount: 20.54 }
+    ]
+
     return (
         <div>
             <div className={styles['modal']}>
@@ -27,6 +40,50 @@ function ClientDetails({ client, closeClientDetails, refreshClients }) {
                         <button className={styles['decr-credit-button']}>Decrease credit</button>
                         <button className={styles['delete-button']}>Delete</button>
                     </div>
+                </div>
+                <h2 className={styles['list-info-span']}>Purchases</h2>
+                <div className="d-flex justify-content-center flex-wrap">
+                    <div className={styles['input-group']}>
+                        <label htmlFor="select-date">Date:</label>
+                        <input
+                            id="select-date"
+                            type="date"
+                            placeholder="Select date"
+                            className={`form-control ${styles['input-field']}`}
+                        />
+                    </div>
+                    <div className={styles['input-group']}>
+                        <label htmlFor="order-select">Sort by:</label>
+                        <select
+                            id="order-select"
+                            className={`form-control ${styles['input-field']}`}
+                        >
+                            <option value="0">Date (Latest)</option>
+                            <option value="1">Date (Oldest)</option>
+                            <option value="2">Amount (Descending)</option>
+                            <option value="3">Amount (Ascending)</option>
+                        </select>
+                    </div>
+                </div>
+                <div className={`table-responsive ${styles['table-wrapper']}`}>
+                    <table className={styles['table-fill']}>
+                        <thead>
+                            <tr>
+                                <th className={styles['text-left']}>Date</th>
+                                <th className={styles['text-left']}>Amount</th>
+                            </tr>
+                        </thead>
+                        <tbody className={styles['table-hover']}>
+                            {testPurchases.map(p => {
+                                return (
+                                    <tr>
+                                        <td>{p.date}</td>
+                                        <td>{p.amount}</td>
+                                    </tr>
+                                )
+                            })}
+                        </tbody>
+                    </table>
                 </div>
             </div>
             <div className={styles['backdrop']} />
