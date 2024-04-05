1a4408c1a2c36e1d13f5cfd5194c87589468c478
Stilian Stefanov <106154612+stilianstefanov@users.noreply.github.com>
2024-04-05 12:44:10 +0300

    Added client info, added buttons

--- client/src/components/client/ClientDetails/ClientDetails.js
+++ client/src/components/client/ClientDetails/ClientDetails.js
@@ -4,7 +4,30 @@ function ClientDetails({ client, closeClientDetails, refreshClients }) {
     return (
         <div>
             <div className={styles['modal']}>
-                {client.name}
+                <div className={styles['flex-container']}>
+                    <ul className={styles['list-info']}>
+                        <li>
+                            <p>Name: {client.name}</p>
+                        </li>
+                        <li>
+                            <p>Surname: {client.surname}</p>
+                        </li>
+                        <li>
+                            <p>Lastname: {client.lastName}</p>
+                        </li>
+                        <li>
+                            <p>Current credit: {client.currentCredit}</p>
+                        </li>
+                        <li>
+                            <p>Credit limit: {client.creditLimit}</p>
+                        </li>
+                    </ul>
+                    <div className={styles['buttons-container']}>
+                        <button className={styles['update-button']}>Update</button>
+                        <button className={styles['decr-credit-button']}>Decrease credit</button>
+                        <button className={styles['delete-button']}>Delete</button>
+                    </div>
+                </div>
             </div>
             <div className={styles['backdrop']} />
         </div>
