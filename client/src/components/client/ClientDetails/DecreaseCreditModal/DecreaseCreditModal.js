6a9b6a48dc3c0c5cc36ccc54082f73365be8105e
Stilian Stefanov <106154612+stilianstefanov@users.noreply.github.com>
2024-04-10 12:26:28 +0300

    Implemented html structure and styles

--- client/src/components/client/ClientDetails/DecreaseCreditModal/DecreaseCreditModal.js
+++ client/src/components/client/ClientDetails/DecreaseCreditModal/DecreaseCreditModal.js
@@ -1,7 +1,32 @@
-import style from './DecreaseCreditModal.module.css'
+import styles from './DecreaseCreditModal.module.css'
 
-function DecreaseCreditModal ({ clientId, closeModal, refreshClients}) {
+function DecreaseCreditModal({ clientId, closeModal, refreshClients }) {
+    return (
+        <div>
+            <div className={styles["container"]}>
+                <h1 className={styles["header"]}>Decrease current credit</h1>
+                <form>
+                    <div className={styles['input-group']}>
+                        <label htmlFor="amount-input">Amount:</label>
+                        <input
+                            id="amount-input"
+                            placeholder="Enter amount to decrease"
+                            className={styles["input"]}
 
+                        />
+                    </div>
+                    <div className={styles['buttons-container']}>
+                        <button className={styles['button-cancel']} onClick={closeModal}>
+                            Cancel
+                        </button>
+                        <button type="submit" className={styles["button-confirm"]}>
+                            Confirm
+                        </button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    );
 }
 
 export default DecreaseCreditModal;
