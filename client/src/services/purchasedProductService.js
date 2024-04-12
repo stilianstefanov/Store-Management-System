91b4e8d01822128097e5628c86064f832880b587
Stilian Stefanov <106154612+stilianstefanov@users.noreply.github.com>
2024-04-12 12:13:25 +0300

    Update purchasedProductService.js

--- client/src/services/purchasedProductService.js
+++ client/src/services/purchasedProductService.js
@@ -8,3 +8,10 @@ export const GetAll = async (clientId, purchaseId) => {
     const response = await axios.get(`${baseUrl}/${clientId}/purchases/${purchaseId}/PurchasedProducts`, config);
     return response.data;
 };
+
+export const Delete = async (clientId, purchaseId, purchasedPruductId) => {
+    const config = { headers: { Authorization: `Bearer ${sessionStorage.getItem('token')}` } };
+
+    const response = await axios.delete(`${baseUrl}/${clientId}/purchases/${purchaseId}/PurchasedProducts/${purchasedPruductId}`, config);
+    return response.data;
+};
