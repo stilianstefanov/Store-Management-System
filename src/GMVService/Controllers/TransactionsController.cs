edb2ada5a855b1df5346652b4386a75c480d3de4
Stilian Stefanov <106154612+stilianstefanov@users.noreply.github.com>
2024-04-19 13:16:27 +0300

    Created GMV service

---
+++ src/GMVService/Controllers/TransactionsController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GMVService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TransactionsController : ControllerBase
+    {
+    }
+}
