7d4d98352aef300dcb0af7f0074f2fbd504e12ec
Stilian Stefanov <106154612+stilianstefanov@users.noreply.github.com>
2024-04-23 13:22:36 +0300

    Implemented initial html structure and styles

--- client/src/components/account/accountDetails/UpdadeProfileModal/UpdateProfileModal.js
+++ client/src/components/account/accountDetails/UpdadeProfileModal/UpdateProfileModal.js
@@ -1,7 +1,55 @@
 import styles from './UpdateProfileModal.module.css';
+import { toast } from 'react-toastify';
+import { useNavigate } from 'react-router-dom';
+import { useAuth } from '../../../../context/AuthContext';
+import { useState } from 'react';
 
-function UpdateProfileModal() {
+function UpdateProfileModal({ closeModal }) {
+    const [userName, setUserName] = useState("");
+    const [email, setEmail] = useState("");
+    const [companyName, setCompanyName] = useState("");
+    const [validationErrors, setValidationErrors] = useState({});
+    const { logout } = useAuth();
+    const navigate = useNavigate();
 
+    return (
+        <div className={styles["Auth-container"]}>
+            <div className={styles["Auth-card"]}>
+                <h1 className={styles["Auth-header"]}>Update Profile</h1>
+                <form>
+                    <input
+                        placeholder="Email"
+                        className={styles["Auth-input"]}
+                        value={email}
+
+                    />
+                    {validationErrors.email && <p className={styles["Error-message"]}>{validationErrors.email}</p>}
+                    <input
+                        placeholder="Username"
+                        className={styles["Auth-input"]}
+                        value={userName}
+
+                    />
+                    {validationErrors.userName && <p className={styles["Error-message"]}>{validationErrors.userName}</p>}
+                    <input
+                        placeholder="Company name"
+                        className={styles["Auth-input"]}
+                        value={companyName}
+
+                    />
+                    {validationErrors.companyName && <p className={styles["Error-message"]}>{validationErrors.companyName}</p>}
+                    <button type="submit" className={styles["Auth-button"]}>
+                        Confirm
+                    </button>
+                    <button
+                        className={styles['close-button']}
+                        onClick={closeModal} >
+                        Close
+                    </button>
+                </form>
+            </div>
+        </div>
+    );
 }
 
 export default UpdateProfileModal;
