44d4619dfeb4be238bd73feb8a89b2ee800167ed
Stilian Stefanov <106154612+stilianstefanov@users.noreply.github.com>
2024-04-25 12:21:58 +0300

    Implemented ClaimsPrincipal and Controller extensions

---
+++ src/GMVService/Common/ExceptionMessages.cs
@@ -0,0 +1,7 @@
+﻿namespace GMVService.Common
+{
+    public static class ExceptionMessages
+    {
+        public const string GeneralErrorMessage = "An error occurred while processing your request. Please try again later.";
+    }
+}
---
+++ src/GMVService/Utilities/Extensions/ControllerExtensions.cs
@@ -0,0 +1,22 @@
+﻿using static Azure.Core.HttpHeader;
+
+namespace GMVService.Utilities.Extensions
+{
+    using Enums;
+    using Microsoft.AspNetCore.Mvc;
+    using static Common.ExceptionMessages;
+
+    public static class ControllerExtensions
+    {
+        public static IActionResult Error(this ControllerBase controller,
+            ErrorType errorType = ErrorType.Internal, string errorMessage = GeneralErrorMessage)
+        {
+            return errorType switch
+            {
+                ErrorType.NotFound => controller.NotFound(errorMessage),
+                ErrorType.BadRequest => controller.BadRequest(errorMessage),
+                _ => controller.StatusCode(StatusCodes.Status500InternalServerError, new { Error = GeneralErrorMessage })
+            };
+        }
+    }
+}
