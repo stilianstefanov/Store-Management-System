a63d97942fe901661fe6acf18b68645e3fef970c
Stilian Stefanov <106154612+stilianstefanov@users.noreply.github.com>
2024-04-30 13:14:01 +0300

    Created TransactionRepository.cs

---
+++ src/GMVService/Data/Contracts/ITransactionRepository.cs
@@ -0,0 +1,11 @@
+﻿namespace GMVService.Data.Contracts
+{
+    using Models;
+
+    public interface ITransactionRepository
+    {
+        IQueryable<Transaction> GetAllAsync(string userId);
+
+        Task AddAsync(Transaction transaction);
+    }
+}
---
+++ src/GMVService/Data/TransactionRepository.cs
@@ -0,0 +1,18 @@
+﻿namespace GMVService.Data
+{
+    using Contracts;
+    using Models;
+
+    public class TransactionRepository : ITransactionRepository
+    {
+        public IQueryable<Transaction> GetAllAsync(string userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task AddAsync(Transaction transaction)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
