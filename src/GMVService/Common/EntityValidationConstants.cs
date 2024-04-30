6a8f548e62678ce3613fb932ab3fd63897391610
Stilian Stefanov <106154612+stilianstefanov@users.noreply.github.com>
2024-04-30 13:19:51 +0300

    Added UserId property

---
+++ src/GMVService/Common/EntityValidationConstants.cs
@@ -0,0 +1,11 @@
+﻿namespace GMVService.Common
+{
+    public static class EntityValidationConstants
+    {
+        public const string AmountMinValue = "0.01";
+
+        public const string AmountMaxValue = "999999999";
+
+        public const int UserIdMaxLength = 50;
+    }
+}
--- src/GMVService/Data/Models/Transaction.cs
+++ src/GMVService/Data/Models/Transaction.cs
@@ -2,6 +2,7 @@
 {
     using System.ComponentModel.DataAnnotations;
     using Enums;
+    using static Common.EntityValidationConstants;
 
     public class Transaction
     {
@@ -13,10 +14,17 @@
         [Key]
         public Guid Id { get; set; }
 
+        [Required]
         public DateTime DateTime { get; set; }
 
+        [Required]
         public decimal Amount { get; set; }
 
+        [Required]
         public TransactionType Type { get; set; }
+
+        [Required]
+        [MaxLength(UserIdMaxLength)]
+        public string UserId { get; set; } = null!;
     }
 }
