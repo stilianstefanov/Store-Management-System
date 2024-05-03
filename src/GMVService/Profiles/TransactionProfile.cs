73489935f9834cafb4e20695815a360cf5f7bacc
Stilian Stefanov <106154612+stilianstefanov@users.noreply.github.com>
2024-05-03 15:16:15 +0300

    Fixed minor issue

--- src/GMVService/Profiles/TransactionProfile.cs
+++ src/GMVService/Profiles/TransactionProfile.cs
@@ -14,7 +14,9 @@
                 .ForMember(dest => dest.Type, opt => opt.MapFrom(src => src.Type.ToString()));
 
             CreateMap<TransactionCreateDto, Transaction>()
-                .ForMember(dest => dest.Type, opt => opt.MapFrom(src => Enum.Parse<TransactionType>(src.TransactionType)));
+                .ForMember(dest => dest.Type,
+                    opt => opt.MapFrom(src => Enum.Parse<TransactionType>(src.TransactionType)))
+                .ForMember(dest => dest.Amount, opt => opt.MapFrom(src => src.TotalAmount));
         }
     }
 }
