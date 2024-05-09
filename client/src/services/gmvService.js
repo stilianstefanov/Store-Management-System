1e46d5d69dfb7744bcecb9e5bc7ec5a56a73eb2f
Stilian Stefanov <106154612+stilianstefanov@users.noreply.github.com>
2024-05-09 16:07:38 +0300

    Created gmvService.js

---
+++ client/src/services/gmvService.js
@@ -0,0 +1,13 @@
+import axios from "axios";
+
+export const GetAll = async (params) => {
+    const url = new URL('http://acme.com/api/Transactions');
+    const config = { headers: { Authorization: `Bearer ${sessionStorage.getItem('token')}` } };
+
+    if (params) Object.entries(params).forEach(([key, value]) => {
+        if (value) url.searchParams.append(key, value);
+    });
+
+    const response = await axios.get(url, config);
+    return response.data;
+};
