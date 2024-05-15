ff299d822646366b227d556477a78f8ba268690b
Stilian Stefanov <106154612+stilianstefanov@users.noreply.github.com>
2024-05-15 11:57:29 +0300

    Implemented initial react-i18next

--- client/src/index.js
+++ client/src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { I18nextProvider } from 'react-i18next';
+import i18n from './i18n';
 import { BrowserRouter } from 'react-router-dom';
 import { AuthProvider } from './context/AuthContext';
 
@@ -9,7 +11,9 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
     <AuthProvider>
+      <I18nextProvider i18n={i18n}>
         <App />
+      </I18nextProvider>
     </AuthProvider>
   </BrowserRouter>
 );
